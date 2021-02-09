82ffe51dcc9392096f03f5b3c81fd6c39229e688
DinalbaNavas <dinalba.navas@correo.unimet.edu.ve>
2021-02-09 14:22:21 -0400

    finished hero

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 //Carrusel
 
 document.getElementById("btn1").onclick = function () {
-  var imagen = document.getElementById("slide");
-  imagen.style.backgroundImage = 'url(images/Background1.png)';
-  imagen.style.backgroundSize = "100vw 100vh";
+  var imagen1 = document.getElementById("slide");
+  imagen1.style.background = "url(images/Background1.png)";
+  imagen1.style.backgroundSize = "100vw 100vh"; 
+};
+
+document.getElementById("btn2").onclick = function () {
+  var imagen2 = document.getElementById("slide");
+  imagen2.style.backgroundImage = "url(images/Background2.png)";
+  imagen2.style.backgroundSize = "100vw 100vh";
+};
+
+document.getElementById("btn3").onclick = function () {
+  var imagen3 = document.getElementById("slide");
+  imagen3.style.backgroundImage = "url(images/Background3.png)";
+  imagen3.style.backgroundSize = "100vw 100vh";
+};
+document.getElementById("btn4").onclick = function () {
+  var imagen4 = document.getElementById("slide");
+  imagen4.style.backgroundImage = "url(images/Background4.png)";
+  imagen4.style.backgroundSize = "100vw 100vh";
+};
+document.getElementById("btn5").onclick = function () {
+  var imagen5 = document.getElementById("slide");
+  imagen5.style.backgroundImage = "url(images/Background5.png)";
+  imagen5.style.backgroundSize = "100vw 100vh";
 };
