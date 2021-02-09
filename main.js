c59656a208740a2695043f898bad4dfffcec8066
DinalbaNavas <dinalba.navas@correo.unimet.edu.ve>
2021-02-09 13:54:04 -0400

    first try hero

---
+++ main.js
@@ -0,0 +1,7 @@
+//Carrusel
+
+document.getElementById("btn1").onclick = function () {
+  var imagen = document.getElementById("slide");
+  imagen.style.backgroundImage = 'url(images/Background1.png)';
+  imagen.style.backgroundSize = "100vw 100vh";
+};
